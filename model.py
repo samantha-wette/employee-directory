ffae5ebe9c7ae30b3df033a6975e6da761389f3c
Samantha Wette <samantha.wette@gmail.com>
2022-04-13 16:09:26 -0700

    directory fixed

--- model.py
+++ model.py
@@ -3,90 +3,90 @@ employee_directory = {
         "name": "Joel Burton",
         "email": "joel@hackbrightacademy.com",
         "favorite_food": "peanut-butter pretzels",
-        "position": "Vice President of Education"
+        "position": "Vice President of Education",
     },
 
     "henry": {
         "name": "Henry Chen",
         "email": "henry@hackbrightacademy.com",
         "favorite_food": "Chinese donuts",
-        "position": "Instructor"
+        "position": "Instructor",
     },
 
     "meggie": {
         "name": "Meggie Mahnken",
         "email": "meggie@hackbrightacademy.com",
         "favorite_food": "Foods with single, overpowering flavors",
-        "position": "Instructor"
+        "position": "Instructor",
     },
 
     "bonnie": {
         "name": "Bonnie Schulkin",
         "email": "bonnie@hackbrightacademy.com",
         "favorite_food": "chocolate cake with raspberries",
-        "position": "Instructor"
+        "position": "Instructor",
     },
 
     "kara": {
         "name": "Kara DeWalt",
         "email": "kara@hackbrightacademy.com",
         "favorite_food": "gummy bears",
-        "position": "Lab Instructor"
+        "position": "Lab Instructor",
     },
 
     "meg": {
         "name": "Meg Bishop",
         "email": "meg@hackbrightacademy.com",
         "favorite_food": "waffles",
-        "position": "Lab Instructor"
+        "position": "Lab Instructor",
     },
 
     "katie": {
         "name": "Katie Byers",
         "email": "katiebyers@hackbrightacademy.com",
         "favorite_food": "sushi",
-        "position": "Lab Instructor"
+        "position": "Lab Instructor",
     },
 
     "leslie": {
         "name": "Leslie Castellanos",
         "email": "leslie@hackbrightacademy.com",
         "favorite_food": "dark chocolate",
-        "position": "Teaching Assistant"
+        "position": "Teaching Assistant",
     },
 
     "allyson": {
         "name": "Allyson McKnight",
         "email": "ally@hackbrightacademy.com",
         "favorite_food": "jamon iberico",
-        "position": "Lab Instructor"
+        "position": "Lab Instructor",
     },
 
     "jenn": {
         "name": "Jennifer Griffith-Delgado",
         "email": "jennifer@hackbrightacademy.com",
         "favorite_food": "warm chocolate chip cookies a la mode",
-        "position": "Teaching Assistant"
+        "position": "Teaching Assistant",
     },
 
     "agnes": {
         "name": "Agnes Klimaite",
         "email": "agnes@hackbrightacademy.com",
         "favorite_food": "havarti cheese",
-        "position": "Teaching Assistant"
+        "position": "Teaching Assistant",
     },
 
     "balloonicorn": {
         "name": "Balloonicorn",
         "email": "balloonicorn@hackbrightacademy.com",
         "favorite_food": "magical rainbow cupcakes with iridescent sprinkles",
-        "position": "Support Staff"
+        "position": "Support Staff",
     },
 
     "mel": {
         "name": "Mel",
         "email": "mel@ubermelon.com",
         "favorite_food": "Powerbars",
-        "position": "The Boss"
+        "position": "The Boss",
     }
 }
